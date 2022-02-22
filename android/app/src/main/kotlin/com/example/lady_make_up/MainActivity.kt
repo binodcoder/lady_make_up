19f735e43c9909b06b3c54aa105525f803c17d8b
Binod Bhandari <binodcoder@gmail.com>
2022-02-22 21:12:54 +0545

    make up demo

---
+++ android/app/src/main/kotlin/com/example/lady_make_up/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lady_make_up
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text("click me"),
+      ),
+    );
+  }
+}
